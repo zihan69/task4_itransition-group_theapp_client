9cbbf05e6cd17be6daaf7aa48adac7d5591ce0ec
zihan69 <zihanfaisalahmed@gmail.com>
2025-08-06 15:40:25 +0600

    commit

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -5,7 +5,7 @@ import { useAuth } from '../context/AuthContext';
 const ProtectedRoute = () => {
   const { isAuthenticated, loading } = useAuth();
   if (loading) {
-    return <div>Loading...</div>; // Or a spinner component
+    return <div>Loading...</div>;
   }
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
